d0934e7d8d5016312feb3467b55906c401fb0100
farxa <r.abualhassan@gmail.com>
2024-02-13 17:17:15 +0100

    initial commit

---
+++ src/main/java/com/example/QueueApiApp.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(QueueApiApp.class, args);
+    }
+}
